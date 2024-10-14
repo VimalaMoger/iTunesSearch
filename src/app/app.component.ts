4acb0f7096c93ba00f8516c87febe862e1cabb93
Vimala Moger <vimalamoger@gmail.com>
2024-10-14 10:06:04 -0500

    iTunes Search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Injectable} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { CanActivate, CanActivateChild, Router, RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { UserService } from './search/search.component';
+
+//Router Guards
+
+@Injectable({providedIn:'root'})
+export class OnlyLoggedInUsersGuard implements CanActivate {  
+  constructor(private userService: UserService, private router: Router) {}; 
+
+  canActivate() {
+    console.log("OnlyLoggedInUsers");
+    if (this.userService.isLoggedIn()) { 
+      return true;
+    } else {
+      //window.alert("You don't have permission to view this page"); 
+      this.router.navigate([""]);
+      return false;
+    }
+  }
+} 
+@Injectable({providedIn:'root'}) 
+export class AlwaysAuthChildrenGuard implements CanActivateChild {
+  canActivateChild() {
+    console.log("AlwaysAuthChildrenGuard");
+    return true;
+  }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, RouterOutlet, RouterModule, HeaderComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers:  []
 })
-export class AppComponent {
-  title = 'itunes-app';
-}
+export class AppComponent {}
